32e6e98b5b6ca9b25a85893e5c90b2701fc735dc
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2025-12-02 16:26:07 +0000

    FE/Qt: Treat '--startvm=name' and '--startvm:name' just like '--startvm name' in the hardened main, so we open the support driver when we need it. [simplification] github:gh-418
    svn:sync-xref-src-repo-rev: r171647

--- src/VBox/Frontends/VirtualBox/src/hardenedmain.cpp
+++ src/VBox/Frontends/VirtualBox/src/hardenedmain.cpp
@@ -1,4 +1,4 @@
-/* $Id: hardenedmain.cpp 111977 2025-12-02 15:13:50Z knut.osmundsen@oracle.com $ */
+/* $Id: hardenedmain.cpp 111978 2025-12-02 16:26:07Z knut.osmundsen@oracle.com $ */
 /** @file
  * VBox Qt GUI - Hardened main().
  */
@@ -51,8 +51,8 @@ static int MyStrCmp(const char *psz1, const char *psz2)
 /**
  * Option w/ value matching.
  *
- * @returns -1 on mismatch, 0 on exact match, 1 if there is a separator ('=' or
- *          ':') following in the original string.
+ * @returns -1 on mismatch, 1 on exact match, 0 if there is a value separator
+ *          ('=' or ':') following in the string.
  */
 static int MyStrMatchOptWithValue(const char *pszString, const char *pszOpt)
 {
@@ -63,11 +63,11 @@ static int MyStrMatchOptWithValue(const char *pszString, const char *pszOpt)
         if (ch1 != ch2)
         {
             if (!ch2)
-                return ch1 == ':' || ch1 == '=' ? 1 : -1;
+                return ch1 == ':' || ch1 == '=' ? 0 : -1;
             return -1;
         }
         if (!ch1)
-            return 0;
+            return 1;
     }
 }
 
@@ -95,8 +95,7 @@ int main(int argc, char **argv, char **envp)
         {
             cOptionsLeft -= fStartVM == false;
             fStartVM = true;
-            if (iMatch == 0)
-                i++;
+            i += iMatch;
         }
         else if (   !MyStrCmp(argv[i], "--separate")
                  || !MyStrCmp(argv[i], "-separate"))
